403c4ccd1dcf028321a14debf2b2201b89116e8a
Ester Cortes <002escor@gmail.com>
2023-04-18 17:31:23 +0200

    feat: set up working frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-	return <></>;
+	const [backendData, setBackendData] = useState([{}]);
+
+	useEffect(() => {
+		fetch("/tasks")
+			.then((response) => response.json())
+			.then((data) => {
+				setBackendData(data);
+			});
+	}, []);
+	return (
+		<>
+			<h1>Hello</h1>
+			<p>{JSON.stringify(backendData)}</p>
+		</>
+	);
 };
 export default App;
